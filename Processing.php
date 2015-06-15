1a0c84b2546497b3d872e9aa01de620c1a39978f
h-enjin <kraytenjin@gmail.com>
2015-06-16 01:21:54 +0900

    メインページの表示

--- Processing.php
+++ Processing.php
@@ -1,5 +1,8 @@
 <?php
 // 処理系
 class Processing {
-	
+	// 特殊文字対策
+	function h($strings) {
+		return htmlspecialchars($strings, ENT_QUOTES);
+	}
 }
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Books MEMO</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- [if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif] -->
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+					<span class="sr-only">トグルメニュー</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">Books MEMO</a>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
+		<h1>Hello! ほげ</h1>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
